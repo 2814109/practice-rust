a006d0ff19c1c71e4c4aaee2f5ea15a9bef04d03
2814109 <2814109k@gmail.com>
2024-01-19 02:43:25 +0900

    u

--- tests/cli.rs
+++ tests/cli.rs
@@ -24,3 +24,14 @@ fn true_ok() {
     let mut cmd = Command::cargo_bin("true").unwrap();
     cmd.assert().success();
 }
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("practice-rust").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
--- tests/cli_deps.rs
+++
@@ -1,7 +0,0 @@
-use assert_cmd::Command;
-
-#[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("practice-rust").unwrap();
-    cmd.assert().success();
-}
