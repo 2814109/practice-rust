3bbfe8a96083372ba21a091c531d2cb052cc07cf
2814109 <2814109k@gmail.com>
2024-01-19 02:18:34 +0900

    u

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,20 @@
+use std::process::Command;
+
 #[test]
 fn woks() {
     assert!(true)
+}
+
+#[test]
+fn runs_error() {
+    let mut cmd = Command::new("hello");
+    let res = cmd.output();
+    assert!(res.is_err())
+}
+
+#[test]
+fn runs_success() {
+    let mut cmd = Command::new("ls");
+    let res = cmd.output();
+    assert!(res.is_ok())
 }
